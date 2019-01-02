968e8e2b88223526b879b0fb813bda68ce0a6919
wongming <wangming19911115@gmail.com>
2019-01-02 22:43:22 +0800

    Update evaluate_reverse_polish_notation.py

--- evaluate_reverse_polish_notation.py
+++ evaluate_reverse_polish_notation.py
@@ -1,9 +1,4 @@
-#https://leetcode-cn.com/problems/evaluate-reverse-polish-notation/
-
-"""
-思路：
-    
-"""
+operation_tokens = ["+", "-", "*", "/"]
 
 class Solution(object):
     def evalRPN(self, tokens):
@@ -11,17 +6,29 @@ class Solution(object):
         :type tokens: List[str]
         :rtype: int
         """
+        oper_s = []
+        char_s = []
+        exp = []
+        for ele in tokens:
+            if ele in operation_tokens:
+                s1 = char_s.pop()
+                s2 = char_s.pop()
+                if ele == "+":
+                    val = s2 + s1
+                elif ele == "-":
+                    val = s2 - s1
+                elif ele == "*":
+                    val = s2 * s1
+                elif ele == "/":
+                    if s1 * s2 > 0:
+                        val = s2 / s1
+                    else:
+                        val = (-1 * s2) / s1 * (-1)
 
-if __name__ == '__main__':
-    tokens = ["2", "1", "+", "3", "*"]
-    val = 9
-    print Solution().evalRPN(tokens) == '((2 + 1) * 3) = 9'   
- 
-    tokens = ["4", "13", "5", "/", "+"]
-    val = 6
-    out = '(4 + (13 / 5)) = 6'
-    print Solution().evalRPN(tokens) == out 
- 
-    tokens = ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", \
-        "+"]
-    val == 22
+                char_s.append(val)
+            else:
+                ele = int(ele)
+                char_s.append(ele)
+        return char_s.pop()
+print Solution().evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"])
+print Solution().evalRPN(["4","13","5","/","+"])
